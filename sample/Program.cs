9873777260e014402b950eb9dde876649ba95c46
Ahmed Ali <ahmedmarzouk.ir@gmail.com>
2022-08-08 16:35:56 +0100

    Add a way to visualize the call stack in GUI

--- sample/Program.cs
+++ sample/Program.cs
@@ -1,14 +1,37 @@
-﻿Console.WriteLine("What is your name?");
-var currentDate = DateTime.Now;
- Console.WriteLine($"{Environment.NewLine}Hello, Ahmed , on {currentDate:d} at {currentDate:t}!");
-Console.Write($"{Environment.NewLine} Press any key to exit...");
-Console.WriteLine($"{Environment.NewLine}Hello, Ahmed , on {currentDate:d} at {currentDate:t}!");
-Console.Write($"{Environment.NewLine} Press any key to exit...");
-Console.WriteLine($"{Environment.NewLine}Hello, Ahmed , on {currentDate:d} at {currentDate:t}!");
-Console.Write($"{Environment.NewLine} Press any key to exit...");
- Console.WriteLine($"{Environment.NewLine}Hello, Ahmed , on {currentDate:d} at {currentDate:t}!");
- Console.Write($"{Environment.NewLine} Press any key to exit...");
-Console.WriteLine($"{Environment.NewLine}Hello, Ahmed , on {currentDate:d} at {currentDate:t}!");
-Console.Write($"{Environment.NewLine} Press any key to exit...");
-Console.WriteLine($"{Environment.NewLine}Hello, Ahmed , on {currentDate:d} at {currentDate:t}!");
-Console.Write($"{Environment.NewLine} Press any key to exit...");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int SumPostOrder(BTNode? node)
+        {
+            if (node == null)
+            {
+                return 0;
+            }
+
+            var x = SumPostOrder(node.Left);
+            var y = SumPostOrder(node.Right);
+            return x + y + node.Data;
+        }
+
+        var bt = new BTNode(5);
+        bt.Left = new BTNode(2);
+        bt.Right = new BTNode(3);
+
+        SumPostOrder(bt);
+    }
+}
+
+class BTNode
+    {
+        public int Data;
+        public BTNode? Left;
+        public BTNode? Right;
+
+        public BTNode(int data = 0, BTNode? left = null, BTNode? right = null)
+        {
+            Data = data;
+            Left = left;
+            Right = right;
+        }
+    }
--- src/extension.ts
+++ src/extension.ts
@@ -15,7 +15,7 @@ export function activate(context: vscode.ExtensionContext) {
 		const panel = vscode.window.createWebviewPanel(
 			'CallStackNinja',
 			'Call Stack Ninja', 
-			vscode.ViewColumn.One, 
+			vscode.ViewColumn.Two, 
 			{
 				enableScripts: true
 			  }
@@ -27,17 +27,17 @@ export function activate(context: vscode.ExtensionContext) {
 			  return {
 				onWillReceiveMessage: m => {
 					// console.log(`> ${JSON.stringify(m, undefined, 2)}`);
+
 				},
 				onDidSendMessage: m => {
 					
 					var eventStr = JSON.stringify(m, undefined, 2);
 
-					//console.log(`< ${eventStr}`);
+					console.log(`${eventStr}`);
 
 					if(m.type === "response" && m.command === "stackTrace")
 					{
 						panel.webview.html = getWebviewContent(eventStr);
-						console.log(`< ${eventStr}`);
 					}
 				}
 			  };
@@ -49,47 +49,97 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(disposable);
 }
 
+
 function getWebviewContent(sfs?: any) {
 
 	if(!sfs)
 	{
-		return `<!DOCTYPE html>
-  <html lang="en">
-  <head>
-	  <meta charset="UTF-8">
-	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	  <title>Call Stack Ninja</title>
-  </head>
-  <body>
-      <h1>No Stack Trace To Draw</h1>
-  </body>
-  </html>`;
+		return  `<!DOCTYPE html>
+		<html lang="en">
+		<head>
+			<meta charset="UTF-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<title>Cat Coding</title>
+		</head>
+		<body>
+			<img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif" width="300" />
+			<h1 id="lines-of-code-counter">0</h1>
+		
+			<script>
+				const counter = document.getElementById('lines-of-code-counter');
+		
+				let count = 0;
+				setInterval(() => {
+					counter.textContent = count++;
+				}, 100);
+			</script>
+		</body>
+		</html>`;
 	}else{
 		var json = JSON.parse(sfs);
-		var stackFrames = json.body.stackFrames;
+		var stackFrames = JSON.stringify(json.body.stackFrames);
 
 		return `<!DOCTYPE html>
 		<html lang="en">
 		<head>
 			<meta charset="UTF-8">
 			<meta name="viewport" content="width=device-width, initial-scale=1.0">
-			<title>Call Stack Ninja</title>
-			</head>
+			<title>Cat Coding</title>
+		</head>
 		<body>
-			<h6>${stackFrames}</h6>
-			<canvas id="stackFramesContainer" width="300" height="500" style="border:1px solid #d3d3d3;">Your browser does not support the HTML5 canvas tag.</canvas>
+		<h2 align = "center">Stack Visualization</h2>
+		<svg  id = "stackFramesSvg" height = "500" xmlns = "http://www.w3.org/2000/svg">
+		<rect height="500" width="200" />
+		</svg>
+		
+		<sf id="sf-id" data-frames='${stackFrames}' ></sf>
+		
+			<script>
+
+			var svg = document.getElementById("stackFramesSvg"); 
+
+			var maxHeight = 500;
+			var frameHeight =50;
+			var frameColor = "red";
+
+			const framesObj = document.querySelector('#sf-id');
+
+			console.log(framesObj.dataset.frames);
+
+			var jsonFrames = JSON.parse(framesObj.dataset.frames).reverse();
+
+			 for (let frame of jsonFrames) {
+			 
+			 var currentFrameHeight = maxHeight - frameHeight;
+			 
+			 var frameSlot = document.createElementNS("http://www.w3.org/2000/svg", "rect"); 
+			frameSlot.setAttribute("height",50); 
+			frameSlot.setAttribute("width",200); 
+			frameSlot.setAttribute("y",currentFrameHeight); 
+			frameSlot.setAttribute("fill", frameColor);
+			frameColor = frameColor === "red" ? "blue" : "red";
+			frameHeight += 50;
+				var txt = document.createElementNS("http://www.w3.org/2000/svg", "text");
+					txt.setAttribute("y", currentFrameHeight+40);
+					txt.setAttribute("x", 25);
+					txt.setAttribute("height",50); 
+					txt.setAttribute("width", 150);
+					txt.style.fill = "white";
+					txt.style.fontFamily = "Calibri";
+					txt.style.fontSize = "15";
+					txt.style.fontWeight = 300;
+					txt.textContent  = frame.name.split('.').pop();
+					var group = document.createElementNS("http://www.w3.org/2000/svg", "g");
+					group.appendChild(frameSlot);
+					group.append(txt);
+					svg.appendChild(group);
+			 }
+		
+			</script>
 		</body>
-		<script>
-		var c = document.getElementById("stackFramesContainer");
-		var ctx = c.getContext("2d");
-		ctx.beginPath();
-		ctx.rect(10, 190, 65, 300);
-		ctx.stroke();
-		for (let frame of stackFrames) {
-			<h3>frame</h3>
-		}  
-        </script>
 		</html>`;
+
+	
 	}
 
   }
